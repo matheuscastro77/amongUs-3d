c58a2c295b6d9c6073b1d9b8322a3db3978d58ac
matheuscastro77 <matheustvcastro1997@gmail.com>
2023-06-12 14:33:06 -0300

    amongus 3d webxr

--- src/components/ARComponent.js
+++ src/components/ARComponent.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { ARButton, Controllers, Hands, VRButton, XR } from '@react-three/xr'
 import { Canvas } from '@react-three/fiber'
+import { Amongus } from './Model'
+import { OrbitControls } from '@react-three/drei'
 
 
 const ARComponent = () => {
   return (
-    <>
-    <VRButton />
-    <Canvas>
-      <XR>
-        <Controllers />
+    <div
+      className='container3D'
+      style={{ width: '100%', height: '100vh' }}>
+      <VRButton />
+      <Canvas camera={{position: [0.1, 0.1, 0.1] }}>
+        <directionalLight color={"#ffffff"}
+          position={[5, 5, 5]} />
+        <XR>
         <Hands />
-        <mesh>
-          <boxGeometry />
-          <meshBasicMaterial color="blue" />
-        </mesh>
-      </XR>
-    </Canvas>
-  </>
+          <Controllers />
+          <Suspense fallback={null}>
+            <Amongus />
+          </Suspense>
+          <OrbitControls target={[0, 0, -0.3]} />
+        </XR>
+      </Canvas>
+    </div>
   )
 }
 
