72dbdd921679caf20bd5ba623e5159b9c45c3587
matheuscastro77 <matheustvcastro1997@gmail.com>
2023-06-12 15:47:17 -0300

    teste

--- src/components/ARComponent.js
+++ src/components/ARComponent.js
@@ -7,10 +7,23 @@ import { OrbitControls } from '@react-three/drei'
 
 const ARComponent = () => {
   return (
-    <div
-      className='container3D'
-      style={{ width: '100%', height: '100vh' }}>
-      <ARButton />
+    <>
+       <ARButton />
+      <Canvas camera={{ position: [0, 2, 0] }}>
+        <XR referenceSpace="local-floor">
+          <color attach="background" args={["#111"]} />
+          <ambientLight intensity={2} />
+          <pointLight position={[20, 10, -10]} intensity={2} />
+          <primitive object={new THREE.AxesHelper(2)} />
+          <primitive object={new THREE.GridHelper(20, 20)} />
+          <OrbitControls />
+
+          <Box key="companionCube">
+            <meshPhongMaterial color="#440066" />
+          </Box>
+        </XR>
+      </Canvas>
+      {/* <ARButton />
       <Canvas vr="true" camera={{position: [0.1, 0.1, 0.1] }}>
         <directionalLight color={"#ffffff"}
           position={[5, 5, 5]} />
@@ -20,8 +33,8 @@ const ARComponent = () => {
             <Amongus />
           </Suspense>
         </XR>
-      </Canvas>
-    </div>
+      </Canvas> */}
+    </>
   )
 }
 
