2b67ecfc2f1b37f09e4b593a036b61e7592e9099
matheuscastro77 <matheustvcastro1997@gmail.com>
2023-06-14 09:10:33 -0300

    styling and refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import ARComponent from './components/ARComponent';
 
 function App() {
--- src/components/ARComponent.js
+++ src/components/ARComponent.js
@@ -1,22 +1,19 @@
 import React, { Suspense } from 'react'
-import { ARButton, Controllers, Hands, XR, XRController } from '@react-three/xr'
+import { ARButton, Controllers, XR } from '@react-three/xr'
 import { Canvas } from '@react-three/fiber'
 import { Amongus } from './Model'
-import { Box, OrbitControls } from '@react-three/drei'
-import * as THREE from 'three'
+import { OrbitControls } from '@react-three/drei'
 
 
 const ARComponent = () => {
   return (
-    <>
       <>
         <ARButton />
-        <Canvas >
+        <Canvas camera={{ position: [0.05, 0.05, 0.05]}}       style={{ height:"100vh", width: '100%'}}>
         <directionalLight color={"#ffffff"}
           position={[5, 5, 5]} />
           <XR referenceSpace="local">
-
-            <Suspense fallback={null} >
+          <Suspense fallback={null} >
             <Amongus />
           </Suspense>
           <Controllers />
@@ -24,18 +21,6 @@ const ARComponent = () => {
           </XR>
         </Canvas>
       </>
-      {/* <ARButton />
-      <Canvas vr="true" camera={{position: [0.1, 0.1, 0.1] }}>
-        <directionalLight color={"#ffffff"}
-          position={[5, 5, 5]} />
-        <XR referenceSpace="local-floor">
-        <OrbitControls />
-          <Suspense fallback={null}>
-            <Amongus />
-          </Suspense>
-        </XR>
-      </Canvas> */}
-    </>
   )
 }
 
